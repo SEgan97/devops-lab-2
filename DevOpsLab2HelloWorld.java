7fb1c030d737b46230677be6ae07a176e17ddb83
Stephen <stephen.egan088@gmail.com>
2023-09-29 11:17:14 +0100

    Second Commit

--- DevOpsLab2HelloWorld.java
+++ DevOpsLab2HelloWorld.java
@@ -1,5 +1,7 @@
-class HelloWorld {
+public class DisplayNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
